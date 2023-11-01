960fc7932236b6e2ae3bdeadd91a6825a03b833e
Alan <alan.aguirre666a@gmail.com>
2023-11-01 12:01:12 -0600

    esta es mi primera version

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+
+import MiComponente from './components/MiComponente.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="facelift" class="logo" src="./assets/facelift.jpg" width="125" height="125" />
+
+    <div class="wrapper">
+      <MiComponente msg="EL MEJOR ALBUM DE ALICE IN CHAINS" />
+    </div>
+  </header>
+</template>
+
+<component :is="componentId">
+
+
+
+
+
+</component>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      EL MEJOR ALBUM DE ALICE IN CHAINS LANZADO EN 1990
+   
+    </h3>
+  </div>
+</template>
+
